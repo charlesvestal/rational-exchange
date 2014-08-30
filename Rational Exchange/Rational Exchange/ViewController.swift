6d61fef9761435ca693a677ed829a8c7c65dad4c
Charles Vestal <charlesv@gmail.com>
2014-08-30 17:57:50 -0500

    Removing extraneous UI in advance of implementing picker

--- Rational Exchange/Rational Exchange/ViewController.swift
+++ Rational Exchange/Rational Exchange/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBOutlet weak var foreignTaxField: UITextField!
-    @IBOutlet weak var foreignTipField: UITextField!
-    @IBOutlet weak var foreignExchangeField: UITextField!
     @IBOutlet weak var foreignCostField: UITextField!
     @IBOutlet weak var homeCostField: UITextField!
     @IBOutlet weak var homeCostLabel: UILabel!
-    @IBOutlet weak var homeTaxField: UITextField!
-    @IBOutlet weak var homeTipField: UITextField!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var foreignPicker: UIPickerView!
+    @IBOutlet weak var homePicker: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,9 +48,6 @@ class ViewController: UIViewController {
     )
     
     func initUI() {
-        foreignTaxField.text = String(format: "%0.2f", tipCalc.foreignTaxRate)
-        foreignTipField.text = String(format: "%0.2f", tipCalc.foreignTipRate)
-        foreignExchangeField.text = String(format: "%0.2f", tipCalc.exchangeRate)
         foreignCostField.text = String(format: "%0.2f", tipCalc.foreignTheyWant)
         homeCostField.text = String(format: "%0.2f", tipCalc.calcShouldFeelLike())
         homeCostLabel.text = String(format: "%0.2f total = %0.2f + %0.2f tip + %0.2f tax",
@@ -62,17 +55,12 @@ class ViewController: UIViewController {
             tipCalc.calcShouldFeelLike(),
             tipCalc.calcShouldTipLike(),
             tipCalc.calcShouldTaxLike())
-        homeTaxField.text = String(format: "%0.4f", tipCalc.homeTaxRate)
-        homeTipField.text = String(format: "%0.2f", tipCalc.homeTipRate)
+       
+        let pickerData: [String] = ["Norway", "USA", "Germany"]
     }
 
     func updateUI()   {
-        tipCalc.foreignTaxRate =  Double((foreignTaxField.text as NSString).doubleValue)
         tipCalc.foreignTheyWant = Double((foreignCostField.text as NSString).doubleValue)
-        tipCalc.exchangeRate =  Double((foreignExchangeField.text as NSString).doubleValue)
-        tipCalc.foreignTipRate = Double((foreignTipField.text as NSString).doubleValue)
-        tipCalc.homeTaxRate =  Double((homeTaxField.text as NSString).doubleValue)
-        tipCalc.homeTipRate =  Double((homeTipField.text as NSString).doubleValue)
         homeCostField.text = NSString (format: "%.2f", tipCalc.calcShouldFeelLike())
         homeCostLabel.text = String(format: "%0.2f total = %0.2f + %0.2f tip + %0.2f tax",
             tipCalc.calcTotalAmount(),
