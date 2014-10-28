fba702eb7bbc2fa4ea0441adacf9772ed85c5421
Charles Vestal <charlesv@gmail.com>
2014-10-27 19:19:57 -0500

    fuck it, hud

--- Rational Exchange/CVCountrySummaryViewController.swift
+++ Rational Exchange/CVCountrySummaryViewController.swift
@@ -70,6 +70,12 @@ class CVCountrySummaryViewController:UIViewController, MFMailComposeViewControll
     }
     
     @IBAction func findMe(sender: AnyObject) {
+        
+        var hud =  MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        hud.detailsLabelText = "Finding Your Location"
+        hud.dimBackground = true
+        hud.hide(true, afterDelay:2.0)
+        
         let containerView = self.view.superview as CVUIContainerView
         if(containerView.isForeign == true){
              MyCLController.setDomain("foreign")
